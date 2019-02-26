0913705cd2bb14eacab7de45f46556f0bec5c6a9
skuhlmann <samkuhlmann@gmail.com>
2019-02-26 15:54:37 -0700

    Adds in etherscan check to ensure disable

--- bots/update-tx-status.js
+++ bots/update-tx-status.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const { getByTokenId, updateBot } = require("../util/dyanamo-queries");
+const EtherScanApi = require("../util/etherscan-api");
 
 module.exports.updateTxStatus = async (event, context) => {
   const reqData = JSON.parse(event.body);
@@ -24,6 +25,28 @@ module.exports.updateTxStatus = async (event, context) => {
   try {
     const getRes = await getByTokenId(event.pathParameters.tokenId);
     const bot = getRes.Items[0];
+
+    console.log(bot.txHash.length);
+
+    if (reqData.disabled && bot.txHash !== null && bot.txHash.length === 66) {
+      console.log("checking etherscan");
+      const api = new EtherScanApi();
+      const txStatus = await api.getTransactionReceipt(bot.txHash);
+      console.log(txStatus);
+
+      //TODO: Unknown if this is a valid pending status
+      const status = txStatus.result ? txStatus.result.status : "pending";
+      if (status !== "0x0") {
+        return {
+          statusCode: 400,
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": process.env.ORIGIN
+          },
+          body: JSON.stringify({ message: "Cannot disable at this time" })
+        };
+      }
+    }
 
     const updateParams = {
       TableName: process.env.DYNAMODB_TABLE,
